2ba18285265bd5f0d8221f2b7274357bd6037736
Tanmay <tanmayprajapat589@gmail.com>
2026-05-29 16:39:57 +0530

    completed global-header full

--- src/components/global/global-header/index.tsx
+++ src/components/global/global-header/index.tsx
@@ -1,15 +1,27 @@
+'use client'
+
 import { WorkSpace } from '@prisma/client'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 type Props = {
-workspace:WorkSpace
+  workspace: WorkSpace
 }
 
-const GlobalHeader = (props: Props) => {
+const GlobalHeader = ({ workspace }: Props) => {
+
+  const pathName = usePathname().split(`/dashboard/${workspace.id}`)[1]
   return (
-    <div>
-      
-    </div>
+    <article className="flex flex-col gap-2">
+      <span className="text-[#707070] text-xs">
+        {workspace.type.toLocaleUpperCase()}
+      </span>
+      <h1 className="text-4xl font-bold">
+        {pathName && !pathName.includes('folder')
+          ? pathName.charAt(0).toUpperCase() + pathName.slice(1).toLowerCase()
+          : 'My Library'}
+      </h1>
+    </article>
   )
 }
 
